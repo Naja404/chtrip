37146d8b1d353f3f78650e88441160dcb2fdd16f
Hisoka <Hisoka@HisokadeMacBook-Pro.local>
2015-09-02 11:25:08 +0800

    upload fix file

--- chtrips/chtrips/ViewControllers/Discovery/DiscoveryViewController.m
+++ chtrips/chtrips/ViewControllers/Discovery/DiscoveryViewController.m
@@ -20,13 +20,15 @@ static NSString * const DISCOVERY_CELL = @"discoveryCell";
 @interface DiscoveryViewController ()<UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate, UIScrollViewDelegate, UITextFieldDelegate>
 
 @property (nonatomic, strong) UITableView *discoveryTV;
-@property (nonatomic, strong) NSMutableDictionary *discoveryTVData;
+@property (nonatomic, strong) NSMutableArray *discoveryTVData;
 @property (nonatomic, strong) UIView *discoveryHV;
 @property (nonatomic, strong) UIScrollView *adScrollView;
 @property (nonatomic, strong) UIPageControl *adPageControl;
 @property (nonatomic, strong) CHAutoSlideScrollView *kvScrollView;
 @property (nonatomic, strong) UIView *searchView;
 @property (nonatomic, strong) UITextField *searchField;
+@property (nonatomic, strong) UIRefreshControl *refreshTV;
+
 
 @end
 
@@ -45,9 +47,31 @@ static NSString * const DISCOVERY_CELL = @"discoveryCell";
     [self setupDiscoveryTV];
     [self setupSearchNav];
     [self setupLogo];
+    [self refresh:self.refreshTV];
     
-
+    
     // Do any additional setup after loading the view.
+}
+
+#pragma mark 获取专辑列表
+
+- (void) getAlbumList {
+    [SVProgressHUD show];
+    NSMutableDictionary *paramter = [NSMutableDictionary dictionary];
+    [paramter setObject:[CHSSID SSID] forKey:@"ssid"];
+    
+    [[HttpManager instance] requestWithMethod:@"Product/albumList"
+                                   parameters:paramter
+                                      success:^(NSDictionary *result) {
+                                          NSLog(@"album list data is %@", result);
+                                          self.discoveryTVData = [[NSMutableArray alloc] initWithArray:[[result objectForKey:@"data"] objectForKey:@"albumList"]];
+                                          [self.discoveryTV reloadData];
+                                          [SVProgressHUD dismiss];
+                                          
+                                      }
+                                      failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                                          [SVProgressHUD dismiss];
+                                      }];
 }
 
 - (void) setupLogo {
@@ -87,7 +111,7 @@ static NSString * const DISCOVERY_CELL = @"discoveryCell";
 }
 
 - (void) setupSearchNav {
-
+    
     self.searchView = [UIView newAutoLayoutView];
     [self.view addSubview:_searchView];
     
@@ -96,7 +120,7 @@ static NSString * const DISCOVERY_CELL = @"discoveryCell";
     [_searchView autoSetDimensionsToSize:CGSizeMake(ScreenWidth, 44)];
     
     _searchView.backgroundColor = [UIColor whiteColor];
-
+    
     UIImageView *bgView = [UIImageView newAutoLayoutView];
     [self.searchView addSubview:bgView];
     
@@ -130,15 +154,19 @@ static NSString * const DISCOVERY_CELL = @"discoveryCell";
     self.discoveryTV = [UITableView newAutoLayoutView];
     [self.view addSubview:_discoveryTV];
     
-//    [_discoveryTV autoPinToTopLayoutGuideOfViewController:self withInset:-65.0];
+    //    [_discoveryTV autoPinToTopLayoutGuideOfViewController:self withInset:-65.0];
     [_discoveryTV autoPinEdge:ALEdgeTop toEdge:ALEdgeTop ofView:self.view withOffset:44];
     [_discoveryTV autoPinEdge:ALEdgeLeft toEdge:ALEdgeLeft ofView:self.view];
     [_discoveryTV autoPinEdge:ALEdgeRight toEdge:ALEdgeRight ofView:self.view];
     [_discoveryTV autoPinEdge:ALEdgeBottom toEdge:ALEdgeBottom ofView:self.view];
-
+    
     
     _discoveryTV.delegate = self;
     _discoveryTV.dataSource = self;
+    
+    self.refreshTV = [[UIRefreshControl alloc] init];
+    [_discoveryTV addSubview:_refreshTV];
+    [_refreshTV addTarget:self action:@selector(refresh:) forControlEvents:UIControlEventValueChanged];
     
     [self.discoveryTV registerClass:[DiscoveryTableViewCell class] forCellReuseIdentifier:DISCOVERY_CELL];
     
@@ -157,7 +185,7 @@ static NSString * const DISCOVERY_CELL = @"discoveryCell";
     }
     
     self.discoveryHV = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 142)];
-
+    
     self.kvScrollView = [[CHAutoSlideScrollView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, 142) animationDuration:5];
     
     [self.discoveryHV removeFromSuperview];
@@ -179,7 +207,7 @@ static NSString * const DISCOVERY_CELL = @"discoveryCell";
 }
 
 - (void) setupDiscoveryTVHeaderView {
-//    self.discoveryHV = [[UIView alloc] initForAutoLayout];
+    //    self.discoveryHV = [[UIView alloc] initForAutoLayout];
     self.discoveryHV = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
     
     self.adScrollView = [UIScrollView newAutoLayoutView];
@@ -201,18 +229,18 @@ static NSString * const DISCOVERY_CELL = @"discoveryCell";
         UIImageView *imgView = [[UIImageView alloc] init];
         imgView.frame = CGRectMake(ScreenWidth * i, 0, ScreenWidth, 200);
         imgView.image = [UIImage imageNamed:[NSString stringWithFormat:@"ad%d.jpg", i + 1]];
-//        NSURL *imageUrl = [NSURL URLWithString:[self.imgArr objectAtIndex:i]];
-//        [imgView setImageWithURL:imageUrl placeholderImage:[UIImage imageNamed:@"productDemo3"]];
+        //        NSURL *imageUrl = [NSURL URLWithString:[self.imgArr objectAtIndex:i]];
+        //        [imgView setImageWithURL:imageUrl placeholderImage:[UIImage imageNamed:@"productDemo3"]];
         
-//                imgView.image = [UIImage imageNamed:[NSString stringWithFormat:@"%@", [self.imgArr objectAtIndex:i]]];
+        //                imgView.image = [UIImage imageNamed:[NSString stringWithFormat:@"%@", [self.imgArr objectAtIndex:i]]];
         
         [_adScrollView addSubview:imgView];
-    
+        
     }
     
     self.adPageControl = [[UIPageControl alloc] init];
     
-//    self.adPageControl = [UIPageControl newAutoLayoutView];
+    //    self.adPageControl = [UIPageControl newAutoLayoutView];
     
     _adPageControl.center = CGPointMake(ScreenWidth / 2, 180);
     _adPageControl.bounds = CGRectMake(0, 0, 16*(3-1)+16, 16);
@@ -222,7 +250,7 @@ static NSString * const DISCOVERY_CELL = @"discoveryCell";
     _adPageControl.enabled = NO;
     
     [_discoveryHV addSubview:_adPageControl];
-
+    
     _discoveryHV.backgroundColor = [UIColor clearColor];
     
     self.discoveryTV.tableHeaderView = _discoveryHV;
@@ -234,7 +262,11 @@ static NSString * const DISCOVERY_CELL = @"discoveryCell";
 }
 
 - (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 10;
+    if ([self.discoveryTVData count] <= 0) {
+        return 0;
+    }else{
+        return [self.discoveryTVData count];
+    }
 }
 
 - (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView {
@@ -242,15 +274,23 @@ static NSString * const DISCOVERY_CELL = @"discoveryCell";
 }
 
 - (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return 294;
+    //    return 294;
+    return ScreenWidth + 13;
 }
 
 
 - (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     DiscoveryTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:DISCOVERY_CELL forIndexPath:indexPath];
-    cell.titleLB.text = @"松本清特惠专场";
-    cell.leftLB.text = @"剩余 20 天";
-    cell.bgImg.image = [UIImage imageNamed:[NSString stringWithFormat:@"ad0%d.jpg", indexPath.row]];
+    
+    NSDictionary *cellData = [[NSDictionary alloc] initWithDictionary:[self.discoveryTVData objectAtIndex:indexPath.row]];
+    
+    NSURL *imageUrl = [NSURL URLWithString:[cellData objectForKey:@"path"]];
+    [cell.bgImg setImageWithURL:imageUrl placeholderImage:[UIImage imageNamed:@"defaultPic.jpg"]];
+    
+    cell.titleLB.text = [cellData objectForKey:@"title"];
+    //    cell.leftLB.text = @"";
+    //    cell.bgImg.image = [UIImage imageNamed:[NSString stringWithFormat:@"ad0%d.jpg", indexPath.row]];
+    //    cell.bgImg.alpha = 0.8;
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
     return cell;
 }
@@ -259,25 +299,12 @@ static NSString * const DISCOVERY_CELL = @"discoveryCell";
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
     DiscoveryDetailViewController *detail = [[DiscoveryDetailViewController alloc] init];
+    NSDictionary *cellData = [[NSDictionary alloc] initWithDictionary:[self.discoveryTVData objectAtIndex:indexPath.row]];
     
-    if (indexPath.row == 1) {
-        detail.webUrl = @"http://api.atniwo.com/web.php";
-    }else{
-        detail.webUrl = @"http://api.atniwo.com/product.html";
-    }
+    detail.webUrl = [NSString stringWithFormat:@"http://api.atniwo.com/Product/showAlbum?aid=%@", [cellData objectForKey:@"aid"]];
+    detail.navigationItem.title = [cellData objectForKey:@"type_name"];
     
-<<<<<<< HEAD
     detail.hidesBottomBarWhenPushed = YES;
-
-//    self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"arrowLeft"] style:UIBarButtonItemStylePlain target:nil action:nil];
-=======
-    // self.hidesBottomBarWhenPushed = YES;
-    detail.hidesBottomBarWhenPushed = YES;
-   
-    self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"arrowLeft"] style:UIBarButtonItemStylePlain target:nil action:nil];
-    
->>>>>>> 158ec7cb634839f61f09de253dee69b5786b4b7f
-//    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"share"] style:UIBarButtonItemStylePlain target:nil action:nil];
     
     [self.navigationController pushViewController:detail animated:YES];
 }
@@ -290,5 +317,10 @@ static NSString * const DISCOVERY_CELL = @"discoveryCell";
     [self.searchField resignFirstResponder];
 }
 
+- (void) refresh:(UIRefreshControl *)control {
+    [control beginRefreshing];
+    [self getAlbumList];
+    [control endRefreshing];
+}
 
 @end
