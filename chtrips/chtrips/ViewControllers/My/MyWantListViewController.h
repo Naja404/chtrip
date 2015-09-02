cc7ad4725f0de929a9b0280d2293cbd614cd0caf
Hisoka <Hisoka@HisokadeMacBook-Pro.local>
2015-09-02 10:55:41 +0800

    update other file

---
+++ chtrips/chtrips/ViewControllers/My/MyWantListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyWantListViewController.h
+//  chtrips
+//
+//  Created by Hisoka on 15/8/24.
+//  Copyright (c) 2015年 HSK.ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyWantListViewController : UIViewController
+
+@end
